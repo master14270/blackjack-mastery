f0e93c5fd19cf6e6d1d184a3376f5c3ad468b942
master14270 <jessie.smith14270@gmail.com>
2025-01-26 22:35:16 -0500

    feat: Adds test to verify new deck true count is accurate.

--- main.ts
+++ main.ts
@@ -1,12 +1,7 @@
 import BlackjackGame from "./blackjack_game.ts";
 
-export function add(a: number, b: number): number {
-	return a + b;
-}
-
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-	// console.log("Add 2 + 3 =", add(2, 3));
 	const decks = 1;
 	const game = new BlackjackGame(decks);
 	game.printDecks();
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,12 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import BlackjackGame from "./blackjack_game.ts";
 
-Deno.test(function addTest() {
-	assertEquals(add(2, 3), 5);
+Deno.test(function testNewDeckTrueCount() {
+	// Generate a new deck, and verify the true count is always zero.
+	const deck_counts_to_test = [1, 2, 4, 8, 20, 100];
+	for (const deck_count of deck_counts_to_test) {
+		const game = new BlackjackGame(deck_count);
+		const true_count = game.getTrueCount();
+		assertEquals(true_count, 0);
+	}
 });
